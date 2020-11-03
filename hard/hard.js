2a8e400592ae4ffb61e6a10860fcf87edad5dc72
froylan-R <froy.ramz10@gmail.com>
2020-11-02 19:47:15 -0500

    added hard challenge

---
+++ hard/hard.js
@@ -0,0 +1,32 @@
+function add(number1, number2) {
+    var adding = number1 + number2
+    return adding
+}
+
+function subtract(number1, number2) {
+    var subtracting = number1 - number2
+    return subtracting
+}
+
+function divide(number1, number2) {
+    var dividing = number1 / number2
+    return dividing
+}
+
+function mulitply(number1, number2) {
+    var multiplying = number1 * number2
+    return multiplying
+}
+
+
+// var total = add(2, 2)
+// console.log(total)
+
+// var total2 = subtract(10, 5)
+// console.log(total2)
+
+// var total3 = divide(12, 2)
+// console.log(total3)
+
+// var total4 = mulitply(1, 7)
+// console.log(total4)
