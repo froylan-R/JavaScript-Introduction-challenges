5870a5caa42db9131349378141fa2ad0772db882
froylan-R <froy.ramz10@gmail.com>
2020-11-02 19:48:50 -0500

    added medium challenge

---
+++ medium/medium.js
@@ -0,0 +1,10 @@
+var asking = prompt('hey how are you doing')
+
+if(asking === asking.toUpperCase()) {
+    console.log("ey bruv why yu yellin")
+} 
+else if (asking === asking.toLowerCase()) {
+    console.log('bruv speak up ainit even hear yu')
+} else {
+    console.log(`perfect bruv`)
+}
